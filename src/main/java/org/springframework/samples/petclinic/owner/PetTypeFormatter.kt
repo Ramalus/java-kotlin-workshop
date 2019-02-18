d691ed750136b76882bcb6d1938ac8b0c4492b2a
Ronnie Andrew Magatti <ronniemagatti@gmail.com>
2019-02-17 21:18:46 -0600

    master: Convert PetTypeFormatter

--- src/main/java/org/springframework/samples/petclinic/owner/PetTypeFormatter.kt
+++ src/main/java/org/springframework/samples/petclinic/owner/PetTypeFormatter.kt
@@ -13,50 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.samples.petclinic.owner;
+package org.springframework.samples.petclinic.owner
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.Formatter;
-import org.springframework.stereotype.Component;
-
-import java.text.ParseException;
-import java.util.Collection;
-import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.format.Formatter
+import org.springframework.stereotype.Component
+import java.text.ParseException
+import java.util.*
 
 /**
  * Instructs Spring MVC on how to parse and print elements of type 'PetType'. Starting from Spring 3.0, Formatters have
  * come as an improvement in comparison to legacy PropertyEditors. See the following links for more details: - The
  * Spring ref doc: http://static.springsource.org/spring/docs/current/spring-framework-reference/html/validation.html#format-Formatter-SPI
  * - A nice blog entry from Gordon Dickens: http://gordondickens.com/wordpress/2010/09/30/using-spring-3-0-custom-type-converter/
- * <p/>
+ *
+ *
  *
  * @author Mark Fisher
  * @author Juergen Hoeller
  * @author Michael Isvy
  */
 @Component
-public class PetTypeFormatter implements Formatter<PetType> {
+class PetTypeFormatter @Autowired
+constructor(private val pets: PetRepository) : Formatter<PetType> {
 
-    private final PetRepository pets;
-
-    @Autowired
-    public PetTypeFormatter(PetRepository pets) {
-        this.pets = pets;
+    override fun print(petType: PetType, locale: Locale): String {
+        return petType.name
     }
 
-    @Override
-    public String print(PetType petType, Locale locale) {
-        return petType.getName();
-    }
+    @Throws(ParseException::class)
+    override fun parse(text: String, locale: Locale): PetType {
+        this.pets.findPetTypes().forEach { if (it.name == text) return it }
 
-    @Override
-    public PetType parse(String text, Locale locale) throws ParseException {
-        Collection<PetType> findPetTypes = this.pets.findPetTypes();
-        for (PetType type : findPetTypes) {
-            if (type.getName().equals(text)) {
-                return type;
-            }
-        }
-        throw new ParseException("type not found: " + text, 0);
+        throw ParseException("type not found: $text", 0)
     }
 }
